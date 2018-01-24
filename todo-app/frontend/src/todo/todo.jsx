129ee320bce2fae98218d63600d96f25cc8e8131
NEGRAO\Hedio <hedionegrao@gera3.com.br>
2018-01-23 21:19:31 -0300

    Inicio projeto final

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
 import PageHeader from '../templet/pageheader'
+import TodoForm from './tofoForm'
+import TodoList from './todoList'
 
 export default class Todo extends Component {
   render(){
       return (
           <div>
               <PageHeader name='Tarefas' small=' Cadastro'></PageHeader>
+              <TodoForm/>
+              <TodoList/>
            </div>   
 
       )
