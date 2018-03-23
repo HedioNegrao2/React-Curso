d8d771ff8bc6fabf203523fed7c3b074e0dc0747
NEGRAO\Hedio <hedionegrao@gera3.com.br>
2018-03-22 21:20:51 -0300

    rotas

--- my-maney-app/frontend/src/common/template/menutree.jsx
+++ my-maney-app/frontend/src/common/template/menutree.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default props => (
-   <li classNames='treeview'>
+   <li className='treeview'>
         <a href>
                 <i className={`fa fa-${props.icon}`}></i> {props.label}
                 <i className='fa fa-angle-left pull-right'></i>
--- my-maney-app/frontend/src/main/app.jsx
+++ my-maney-app/frontend/src/main/app.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import '../common/template/dependencia'
 
 import Header  from '../common/template/header'
-import Sidebar from '../common/template/sidebar';
+import Sidebar from '../common/template/sidebar'
+import Footer from  '../common/template/footer'
+import Routes from './routes'
 
 export default props => (
     <div className='wraper'>
-        <Header/>
-        <Sidebar/>
+        <Header />
+        <Sidebar />
+        <div className='content-wrapper'>
+            <Routes />
+        </div>
+        <Footer />
     </div>
 
 )
