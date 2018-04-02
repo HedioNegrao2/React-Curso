dfcebdd149f43957af16069302745b16a5994026
NEGRAO\Hedio <hedionegrao@gera3.com.br>
2018-04-01 21:04:53 -0300

    Iniciando o cadastro

--- my-maney-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-maney-app/frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { Component }  from 'react'
 
-export default props => (
-    <div>
-        <h1> Clico de pagamento</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabsContent from '../common/tab/tabsContent'
+
+class BillingCycle extends Component {
+    render(){
+        return(
+            <div>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+                <content>
+                    <Tabs>
+                       <TabsHeader>
+
+                       </TabsHeader>
+                       <TabsContent>
+
+                        </TabsContent>
+                   </Tabs>                        
+                </content>
+            </div>
+        )
+    }
+
+}
+
+export default BillingCycle
